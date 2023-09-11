5a56a8bfac852ac5e78c7c5d363290a5c0d51ab0
Nittin <nittin.sohalia@bettercommerce.io>
2023-09-11 15:25:58 +0530

    Added Bingo Banner Component to show completed patterns

---
+++ src/components/BingoBanner.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps({
+    completed: Number
+});
+
+const banner = [' ', 'B', 'I', 'N', 'G', 'O'];
+</script>
+
+<template>
+    <div class="w-100 d-flex mt-4">
+        <div class="mx-auto btn-group">
+            <button 
+                v-for="i in 5" 
+                :class="'col-1 btn btn-lg btn-outline-primary ' + (i <= completed ? 'active' : '')"
+            > 
+                    {{ banner[i] }} 
+            </button>
+        </div>
+    </div>
+</template>
