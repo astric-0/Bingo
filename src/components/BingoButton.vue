1548c3214791012888c0b8f43d4de2205fc11ec2
Nittin <nittin.sohalia@bettercommerce.io>
2023-09-11 12:21:30 +0530

    Improved button styling

--- src/components/BingoButton.vue
+++ src/components/BingoButton.vue
@@ -1,12 +1,13 @@
 <script setup>
-defineProps({
+let props = defineProps({
     button: Object
 })
 
 const selectedStyle = ' btn-primary disabled '
 const notSelectedStyle = ' btn-outline-primary '
-const commonStyle = ' w-100 btn rounded '
-const classes = commonStyle + (button.isSelected ? selectedStyle : notSelectedStyle)
+const commonStyle = 'w-100 btn rounded '
+const classes = commonStyle + (props.button.isSelected ? selectedStyle : notSelectedStyle)
+
 </script>
 
 <template>
