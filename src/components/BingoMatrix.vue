b615a949c793a75636e3842ba8166ad1acb87bcd
Nittin <nittin.sohalia@bettercommerce.io>
2023-09-12 12:28:58 +0530

    Created component to display bingo matrix

---
+++ src/components/BingoMatrix.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+    matrix: Object,
+    markCube: Function
+})
+</script>
+
+<template>
+    <div class="row justify-content-center" v-for="(row, rindex) in matrix" :key="rindex">
+        <BingoButton 
+            v-for="(col, cindex) in row" 
+            :key="userMatrix[rindex][cindex]" 
+            :button="col" 
+            :rindex="Number(rindex)"
+            :cindex="Number(cindex)" :markCube="markCube" 
+        />
+    </div>
+</template>
