b73b592c4a0cfd55db83558568636f7b35667cdb
Nittin <nittin.sohalia@bettercommerce.io>
2023-09-12 12:48:24 +0530

    Basic modifications to remove bugs

--- src/components/BingoMatrix.vue
+++ src/components/BingoMatrix.vue
@@ -1,4 +1,5 @@
 <script setup>
+import BingoButton from './BingoButton.vue';
 defineProps({
     matrix: Object,
     markCube: Function
@@ -9,10 +10,11 @@ defineProps({
     <div class="row justify-content-center" v-for="(row, rindex) in matrix" :key="rindex">
         <BingoButton 
             v-for="(col, cindex) in row" 
-            :key="userMatrix[rindex][cindex]" 
+            :key="matrix[rindex][cindex]"
             :button="col" 
             :rindex="Number(rindex)"
-            :cindex="Number(cindex)" :markCube="markCube" 
+            :cindex="Number(cindex)" 
+            :markCube="markCube" 
         />
     </div>
 </template>
--- src/views/Game.vue
+++ src/views/Game.vue
@@ -3,8 +3,8 @@ import maker from '../utils/maker';
 import store from '../store';
 import { ref } from 'vue';
 
+import BingoMatrix from '../components/BingoMatrix.vue';
 import BingoBanner from '../components/BingoBanner.vue';
-import BingoButton from '../components/BingoButton.vue';
 
 const userMatrix = ref(maker());
 const computerMatrix = ref(maker());
@@ -28,11 +28,11 @@ const checkPattern = activeMatrix => {
             activeMatrix[j][i].marked == true && vCounter++;
         }
 
-        if (hCounter > vCounter && hCounter > max.maxValue && hCounter < len) 
+        if ((hCounter > vCounter || vCounter == len) && hCounter > max.maxValue && hCounter < len) 
             max = { maxIndex: i, maxValue: hCounter, type: 'h' }
 
-        if (vCounter >= hCounter && vCounter > max.maxValue && vCounter < len)
-            max = { maxIndex: i, maxValue: hCounter, type: 'v' }
+        if ((vCounter >= hCounter || hCounter == len) && vCounter > max.maxValue && vCounter < len)
+            max = { maxIndex: i, maxValue: vCounter, type: 'v' }
 
         hCounter == len && completedPatterns++;
         vCounter == len && completedPatterns++;
@@ -48,9 +48,9 @@ const checkPattern = activeMatrix => {
     diagonalRight == len && completedPatterns++;    
 
     const { maxValue } = max;
-    if (diagonalLeft > maxValue && diagonalLeft > diagonalRight && diagonalLeft < len)
+    if (diagonalLeft >= maxValue && (diagonalLeft > diagonalRight || diagonalRight == len) && diagonalLeft < len)
         max = { maxIndex: -1, maxValue: diagonalLeft, type: 'dl' }
-    if (diagonalRight >= maxValue && diagonalRight > diagonalLeft && diagonalRight < len)
+    if (diagonalRight >= maxValue && (diagonalRight >= diagonalLeft || diagonalLeft == len) && diagonalRight < len)
         max = { maxIndex: -1, maxValue: diagonalLeft, type: 'dr' }
 
     const modules = {
@@ -75,7 +75,7 @@ const checkPattern = activeMatrix => {
     const processor = module => {
         for (let i = 0; i < len; i++)
             if(!module.process(i).marked)
-                return module.get(i)
+                return module.get(i);
         return [ -1, -1 ]
     }
 
@@ -85,17 +85,20 @@ const checkPattern = activeMatrix => {
 }
 
 const markCube = (rindex, cindex, letter, player) => {
-    const [ activeMatrix, passiveMatrix, completedPattern ] = (player == 'user') 
-        ? [ userMatrix, computerMatrix, userCompletedPatterns ]
-        : [ computerMatrix, userMatrix, computerCompletedPatterns ]
+    const [ activeMatrix, passiveMatrix, activeCompletedPattern, passiveCompletedPattern ] = (player == 'user') 
+        ? [ userMatrix, computerMatrix, userCompletedPatterns, computerCompletedPatterns ]
+        : [ computerMatrix, userMatrix, computerCompletedPatterns, userCompletedPatterns ]
     ;
 
     activeMatrix.value[rindex][cindex] = getMarker(activeMatrix.value, rindex, cindex);
-
+    const activeCals = checkPattern(activeMatrix.value);
+    activeCompletedPattern.value = activeCals.completedPatterns;
+    
     let breakFlag = false;
-    for (let i = 0; i < passiveMatrix.value.length; i++) {
-        for (let j = 0; j < passiveMatrix.value[i].length; j++) {
-            if (passiveMatrix.value[i][j] == letter) {
+    const len = passiveMatrix.value.length;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len; j++) {
+            if (passiveMatrix.value[i][j].letter == letter) {
                 breakFlag = true;
                 passiveMatrix.value[i][j] = getMarker(passiveMatrix.value, i, j);
                 break;
@@ -104,8 +107,12 @@ const markCube = (rindex, cindex, letter, player) => {
         if (breakFlag) break;
     }
 
-    const cals = checkPattern(activeMatrix.value);
-    completedPattern.value = cals.completedPatterns;
+    const passiveCals = checkPattern(passiveMatrix.value);
+    passiveCompletedPattern.value = passiveCals.completedPatterns;
+    if (player == 'user') {
+        const { row, col } = passiveCals;
+        markCube(row, col, passiveMatrix.value[row][col].letter, 'computer');
+    }    
 }
 
 </script>
@@ -113,18 +120,10 @@ const markCube = (rindex, cindex, letter, player) => {
 <template>
     <div class="container d-flex align-items-center justify-content-center vh-100">
         <div class="w-50">
-            <h4 class="text-center">{{ store.state.username }}</h4>
-            <div class="row justify-content-center" v-for="(row, rindex) in userMatrix" :key="rindex">
-                <BingoButton
-                        v-for="(col, cindex) in row"
-                        :key="userMatrix[rindex][cindex]"
-                        :button="col" 
-                        :rindex="Number(rindex)" 
-                        :cindex="Number(cindex)" 
-                        :markCube="markCube"
-                    />            
-            </div>
+            <h4 class="text-center">{{ store.state.username }}</h4>            
+            <BingoMatrix :matrix="userMatrix" :key="userMatrix" :markCube="markCube" />
             <BingoBanner :completed="userCompletedPatterns" />
+            <BingoMatrix :matrix="computerMatrix" :key="computerMatrix" :markCube="markCube" />
         </div>
     </div>
 </template>
